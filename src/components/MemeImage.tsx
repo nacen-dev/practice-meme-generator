365c0647e35669241efef048d9951e74fd5b3a88
nacen.dev <nacen.dev@gmail.com>
2022-04-06 23:23:16 +0800

    Change top and bottom value

--- src/components/MemeImage.tsx
+++ src/components/MemeImage.tsx
@@ -11,10 +11,10 @@ const MemeImage = (props: Props) => {
   return (
     <div className="w-full relative">
       <img className="w-full" src={props.imageUrl} alt={props.name} />
-      <p className="uppercase font-oswald font-semibold absolute top-2 left-1/2 translate-x-[-50%] text-white text-3xl">
+      <p className="uppercase font-oswald font-semibold absolute top-5 left-1/2 translate-x-[-50%] text-white text-3xl">
         {props.topText}
       </p>
-      <p className="uppercase font-oswald font-semibold absolute bottom-2 left-1/2 translate-x-[-50%] text-white text-3xl">
+      <p className="uppercase font-oswald font-semibold absolute bottom-5 left-1/2 translate-x-[-50%] text-white text-3xl">
         {props.bottomText}
       </p>
     </div>
