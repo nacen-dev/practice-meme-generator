fb06f208627654008941799d30102f513d5e7538
nacen.dev <nacen.dev@gmail.com>
2022-04-06 19:34:23 +0800

    Initial Setup after removing boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        violet: {
+          light: "#A626D3",
+          dark: "#672280",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
